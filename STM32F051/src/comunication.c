a7b595878134aad090eeaae78b2c5c15d9510d47
kumencz <kubape12@gmail.com>
2015-05-06 13:59:25 +0200

    Human readable UART output

--- STM32F051/src/comunication.c
+++ STM32F051/src/comunication.c
@@ -14,14 +14,24 @@
 void uart_send_temps(void)
 {
 	char send_it[100] = {0};
-	sprintf(send_it, "#%d=temp/%d/%d/%d/%d/%d/%d--ADC-uV=%d$\n\r",\
-	(uint32_t)real_time,\
+//	sprintf(send_it, "#%d/%d/%d/%d/%d/%d/%d$\n\r",\
+//	(uint32_t)real_time/10,\
+//	(uint8_t)s_system.s_temp.thermocouple[0],\
+//	(uint8_t)s_system.s_temp.thermocouple[1],\
+//	(uint8_t)s_system.s_temp.thermocouple[2],\
+//	(uint8_t)s_system.s_temp.thermocouple[3],\
+//	(uint8_t)s_system.s_temp.thermocouple_board,\
+//	(uint8_t)(s_system.s_temp.MCU_temp/10));
+	
+	sprintf(send_it, "#Time %ds -> Thermocouplers: %dC,%dC,%dC,%dC ADC: %dC MCU: %dC   ADC voltage=%duV\n\r",\
+	(uint32_t)real_time/10,\
 	(uint8_t)s_system.s_temp.thermocouple[0],\
 	(uint8_t)s_system.s_temp.thermocouple[1],\
 	(uint8_t)s_system.s_temp.thermocouple[2],\
 	(uint8_t)s_system.s_temp.thermocouple[3],\
 	(uint8_t)s_system.s_temp.thermocouple_board,\
 	(uint8_t)(s_system.s_temp.MCU_temp/10),\
-	(uint16_t)s_system.s_power.ADC_uvolt[2]);
+	s_system.s_power.ADC_uvolt[2]);
+	
 	USART_puts(USART1,send_it);
 }
