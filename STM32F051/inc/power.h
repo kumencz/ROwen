610ad3ae3410cd5fa4b114a243e6ab12c7c87b44
kumencz <kubape12@gmail.com>
2016-04-24 18:10:37 +0200

    Triacs power control only in 100 steps.

--- STM32F051/inc/power.h
+++ STM32F051/inc/power.h
@@ -5,16 +5,15 @@
 /* Exported types ------------------------------------------------------------*/
 typedef enum
 {
-	top_heat,
-	bottom_heat,
-	light,
-	fan
+	triac_1 = 0,
+	triac_2,
+	triac_3,
+	triac_4
 }e_triacs;
 /* Exported constants --------------------------------------------------------*/
-
-
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
+uint16_t triac_get_duty(e_triacs triac);
 void triac_set_duty(e_triacs triac, uint8_t duty);
-void triac_modify_duty(e_triacs triac, int16_t count);
+void triac_modify_duty(e_triacs triac, int8_t count);
 #endif /* __POWER_H */
--- STM32F051/src/power.c
+++ STM32F051/src/power.c
@@ -12,49 +12,68 @@
 /* Private variables ---------------------------------------------------------*/
 
 
-void triac_set_duty(e_triacs triac, uint8_t duty)
+uint16_t triac_get_duty(e_triacs triac)
 {
-	//duty = TIM3->ARR*((100-duty)/(100.0f));
-	
 	switch(triac)
 	{
-		case top_heat:
-			TIM3->CCR3 = duty;
+		case triac_1:
+			return ((TIM3->CCR3)/10);
+		case triac_2:
+			return ((TIM3->CCR4)/10);
+		case triac_3:
+			return ((TIM3->CCR1)/10);
+		case triac_4:
+			return ((TIM3->CCR2)/10);
+		default:
+			return (0);
+	}
+}
+
+void triac_set_duty(e_triacs triac, uint8_t duty)
+{
+	uint16_t duty_scaled = duty*10;
+
+	switch(triac)
+	{
+		case triac_1:
+			TIM3->CCR3 = duty_scaled;
 		break;
-		case bottom_heat:
-			TIM3->CCR4 = duty;
+		case triac_2:
+			TIM3->CCR4 = duty_scaled;
 		break;
-		case light:
-			TIM3->CCR1 = duty;
+		case triac_3:
+			TIM3->CCR1 = duty_scaled;
 		break;
-		case fan:
-			TIM3->CCR2 = duty;
+		case triac_4:
+			TIM3->CCR2 = duty_scaled;
 		break;
 	}
 }
 
-void triac_modify_duty(e_triacs triac, int16_t count)
+void triac_modify_duty(e_triacs triac, int8_t count)
 {
-	switch(triac)
+	int16_t count_scaled = count*10;
+
+	if(count_scaled <= 1000 && count_scaled >= -1000)
 	{
-		case top_heat:
-			TIM3->CCR3 += count;
-			if(TIM3->CCR3 > 65000) TIM3->CCR3 = 0;
-			if(TIM3->CCR3 > 1000) TIM3->CCR3 = 1000;
-			number_to_display(TIM3->CCR3/10,0);
-		break;
-		case bottom_heat:
-			TIM3->CCR4 += count;
-			if(TIM3->CCR4 > 65000) TIM3->CCR4 = 0;
-			if(TIM3->CCR4 > 1000) TIM3->CCR4 = 1000;
-			number_to_display(TIM3->CCR4/10,0);
-		break;
-		case light:
-			number_to_display(TIM3->CCR1/10,0);
-		break;
-		case fan:
-			number_to_display(TIM3->CCR2/10,0);
-		break;
+		switch(triac)
+		{
+			case triac_1:
+				if(TIM3->CCR3 + count_scaled <= (TIM3->ARR+1))
+					TIM3->CCR3 += count_scaled;
+			break;
+			case triac_2:
+				if(TIM3->CCR4 + count_scaled <= (TIM3->ARR+1))
+					TIM3->CCR4 += count_scaled;
+			break;
+			case triac_3:
+				if(TIM3->CCR1 + count_scaled <= (TIM3->ARR+1))
+					TIM3->CCR1 += count_scaled;
+			break;
+			case triac_4:
+				if(TIM3->CCR2 + count_scaled <= (TIM3->ARR+1))
+					TIM3->CCR2 += count_scaled;
+			break;
+		}
 	}
-	
 }
